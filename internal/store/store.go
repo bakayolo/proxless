4ac594fe1f2b818f74e38d89c3a3ec36c0f5ca9c
Appréderisse Benjamin <benjamin.apprederisse@gmail.com>
2020-03-22 18:46:42 +0800

    🚧Add Mutex in store (#4)
    * 🐛 Fix issue https://github.com/bappr/kube-proxless/issues/1

--- internal/store/store.go
+++ internal/store/store.go
@@ -3,6 +3,7 @@ package store
 import (
 	"errors"
 	"github.com/rs/zerolog/log"
+	"sync"
 )
 
 type Route struct {
@@ -14,9 +15,12 @@ type Route struct {
 
 var (
 	routesMap = make(map[string]Route)
+	lock      = sync.RWMutex{}
 )
 
 func UpdateRoute(key, service, port, label, namespace string) {
+	lock.Lock()
+	defer lock.Unlock()
 	routesMap[key] = Route{
 		Service:   service,
 		Port:      port,
@@ -26,12 +30,16 @@ func UpdateRoute(key, service, port, label, namespace string) {
 }
 
 func DeleteRoute(keys ...string) {
+	lock.Lock()
+	defer lock.Unlock()
 	for _, key := range keys {
 		delete(routesMap, key)
 	}
 }
 
 func GetRoute(key string) (Route, error) {
+	lock.RLock()
+	defer lock.RUnlock()
 	if r, ok := routesMap[key]; ok {
 		return r, nil
 	}
